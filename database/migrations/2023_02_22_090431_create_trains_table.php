6fa03955ee1782fa34485991ffdbdcbaabb3dd2d
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-22 17:30:40 +0100

    passaggio colonne nella tabella di phpmyadmin con comando migrate

--- database/migrations/2023_02_22_090431_create_trains_table.php
+++ database/migrations/2023_02_22_090431_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->string('orario_di_partenza', 20);
+            $table->string('orario_di_arrivo', 20);
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze', );
+            $table->boolean('in_orario', 50);
+            $table->boolean('cancellato', 50);
             $table->timestamps();
         });
     }
